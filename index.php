37f2ea53a7e1b4c75422a08e87e9d96243e07026
atholtoncompsci <113360329+atholtoncompsci@users.noreply.github.com>
2022-09-27 16:42:24 +0000

    g

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 <head>
   <title>Atholton Computer Science Club</title>
 </head>
-
 <body>
+  
   <h1>Atholton Computer Science Club</h1>
   Meetings in Ms. Rhee's room, B125
   <br><br>First and third Tuesday of every month
   <br>2:10-3 PM
   asdfghjkl
+
+  <?php
+  //include('repositories/raidertime-22/calc.php');
+?>
 </body>
 </html>
